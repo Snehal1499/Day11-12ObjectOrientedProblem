63aef8af4639162eb49ad792a430a4accb3f1861
Snehal1499 <snehalpatil1718@gmail.com>
2022-04-10 20:14:15 +0530

    Added Object Oriented Program

---
+++ InventoryManagementProgram/StockPortFoili.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace InventoryManagement
+{
+    public class StockPortFoili
+    {
+        public List<Stock> Stocks;
+        public long GrandTotalStockPrice;
+    }
+}
